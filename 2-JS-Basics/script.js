51d0fdd90d9273f8e97a9c14d8d8e9232f2b1c43
Oscar Solis <ohskar2@gmail.com>
2017-10-17 18:48:12 -0500

    Lesson 9

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -1,1 +1,13 @@
- console.log('Hello World');
+var name = 'John';
+console.log(name);
+
+var lastname = 'Smith';
+console.log(lastname);
+
+var age = 26;
+
+console.log(age);
+
+var fullage = true;
+
+console.log(fullage);
