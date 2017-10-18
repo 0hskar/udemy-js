d8737b87cd1acc51defa4e2bbf399e7b7ee2590f
Oscar Solis <ohskar2@gmail.com>
2017-10-17 19:24:13 -0500

    Lesson 11

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -13,7 +13,7 @@ console.log(age);
 var fullage = true;
 console.log(fullage);
 */
-
+/*
 // Lecture: veriables 2
 var name = 'John';
 var age = 26;
@@ -39,5 +39,66 @@ var lastname = prompt('What is the last name?');
 console.log(lastname);
 
 alert( name + ' is ' + age + ' year old ' + job + '. Is he married? ' + married + '.');
+*/
+
+
+
+
+
+// Lecture: operators
+
+var now = 2016;
+var age = 26;
+var birthyear = now - age;
+// 2016 - 52
+// 1964
+
+birthyear = now - age * 2;
+
+console.log(birthyear);
+
+var ageJohn = 30;
+var ageMark = 30;
+
+ageJohn = ageMark = (3 + 5) * 4 - 6;
+//ageJohn = ageMark = 26;
+//ageJohn = 26
+
+ageJohn++;
+ageMark *= 2;
+
+console.log(ageJohn);
+console.log(ageMark);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
