5da157a64b95518093806da9051e0dbe59fb0894
Oscar Solis <ohskar2@gmail.com>
2017-10-17 19:07:20 -0500

    Lesson 10

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -1,3 +1,5 @@
+// Lecture: variables
+/*
 var name = 'John';
 console.log(name);
 
@@ -9,5 +11,33 @@ var age = 26;
 console.log(age);
 
 var fullage = true;
-
 console.log(fullage);
+*/
+
+// Lecture: veriables 2
+var name = 'John';
+var age = 26;
+
+//console.log(name + age);
+//console.log(age  + age);
+
+var job, married;
+
+//console.log(job);
+
+job = 'Teacher';
+married = false;
+
+console.log(name + ' is ' + age + ' year old ' + job + '. Is he married? ' + married + '.');
+
+age = 'thirty six';
+job = 'driver';
+
+console.log(name + ' is ' + age + ' year old ' + job + '. Is he married? ' + married + '.');
+
+var lastname = prompt('What is the last name?');
+console.log(lastname);
+
+alert( name + ' is ' + age + ' year old ' + job + '. Is he married? ' + married + '.');
+
+
