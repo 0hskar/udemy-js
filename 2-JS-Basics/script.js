0d5ed94fdd42249e8d8eec1a8c7ec1477baa21ca
Oscar Solis <ohskar2@gmail.com>
2017-10-22 11:39:11 -0500

    Lesson 18

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -188,7 +188,7 @@ yearsUntilRetirement('Mary', 1948);
 */
 
 // Lecture : Statements and expressions
-
+/*
 function someFun(par) {
 	//code
 }
@@ -206,9 +206,29 @@ var x = 3;
 if( x === 5) {
 	// do something
 }
+*/
 
+// Lecture: Arrays
 
+var names = ['John', 'Jane', 'Mark'];
+var years = new Array(1990, 1969, 1948);
 
+console.log(names[2]);
+names[1] = 'Ben';
+console.log(names);
+
+var john = ['John', 'Smith', 1990, 'designer', false];
+
+john.push('Blue');
+john.unshift('Mr.');
+john.pop();
+john.shift();
+
+console.log(john);
+
+if(john.indexOf('teacher') === -1) {
+	console.log('John is NOT a teacher.');
+}
 
 
 
