d67093f88275e13a0baf188bfc7151bf761cf7ed
Oscar Solis <ohskar2@gmail.com>
2017-10-22 15:55:51 -0500

    Lesson 33

--- 3-How-JS-Works/script.js
+++ 3-How-JS-Works/script.js
@@ -34,7 +34,7 @@ console.log(age);
 
 
 // First scoping example
-
+/*
 
 var a = 'Hello!';
 first();
@@ -54,7 +54,7 @@ function third() {
     //console.log(c);
     console.log(a + d);
 }
-
+*/
 
 
 
@@ -84,6 +84,47 @@ function third() {
 
 ///////////////////////////////////////
 // Lecture: The this keyword
+/*
+console.log(this);
+
+calculateAge(1985);
+
+function calculateAge(year) {
+    console.log(2016 - year);
+    console.log(this);
+}
+*/
+
+var john = {
+    name: 'John',
+    yearOfBirth: 1990,
+    calculateAge: function() {
+        console.log(this);
+        console.log(2016 - this.yearOfBirth)
+        /*
+        function innerFunction(){
+            console.log(this);
+        }
+
+        innerFunction();
+        */
+    }
+}
+
+john.calculateAge();
+
+var mike = {
+    name: 'Mike',
+    yearOfBirth: 1984
+}
+
+mike.calculateAge = john.calculateAge;
+
+mike.calculateAge();
+
+
+
+
 
 
 
